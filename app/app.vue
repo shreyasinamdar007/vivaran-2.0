92a5d8dfcc9dbc29e820d00cdab623c3a6e79996
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2025-12-17 19:28:45 +0530

    vv-2: Added Tailwind and Daisy UI dependencies

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello World
+    </h1>
   </div>
+  <button class="btn btn-secondary">
+    Map
+    <Icon name="tabler:map" size="24" />
+  </button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["./app/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
