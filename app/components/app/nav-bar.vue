5044dab6b5d4eb33867683e929d75bd6a2ca7f90
shreyasinamdar007 <35952498+shreyasinamdar007@users.noreply.github.com>
2026-02-02 21:51:52 +0530

    Setup/vv 3 basic routing (#3)
    * vv-2: Added Tailwind and Daisy UI dependencies
    
    * vv-3-basic-routing: Added routing
    
    * vv-3-basic-routing: Linting Fix

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="navbar bg-teal-600 text-primary-content">
+    <div class="navbar-start h-8">
+      <div class="w-56 h-8 flex items-center justify-center">
+        <img src="/assets/vv.svg" alt="Logo" class="rounded-full mr-2">
+      </div>
+    </div>
+  </div>
+</template>
---
+++ app/pages/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4">
+      Welcome to the Home Page
+    </h1>
+    <p class="text-lg">
+      This is the main content of the index page.
+    </p>
+  </div>
+</template>
